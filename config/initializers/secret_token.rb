32a2c39679918768568abfed66932fff9a0d8434
shimada <snoozer.05+zantare@gmail.com>
2010-10-04 17:20:15 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZantareShimada::Application.config.secret_token = '1ff304e66a421f0c39e9bf916d1f4de4fd901633b5405683eae0e0a5dcca85c6a08fed16d90b0df02a7e5335a6b42294803baf63b724f05356f6b4591652b8d1'
